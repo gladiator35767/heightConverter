646bd2c25614edfc40b22a7252e82720de9c77ab
anthony.battista <anthony.battista@MU-C1-15.mullen.pvt.k12.co.us>
2017-01-27 12:30:08 -0700

    Height converter commit

---
+++ Height Converter/src/heightConverter.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class heightConverter
+	{
+
+		public static void main(String[] args)
+			{
+			double feet = inputFeet();
+			double inches = inputInches();
+			double justInches = convertToInches();
+			double centimeters = convertToCentimeters();
+			double meters = convertToMeters();
+			double convertFeet = outputFeet();
+			double convertInches = outputInches();
+			}
+				
+	    public static double inputFeet()
+	    	{
+		     Scanner userInput =new Scanner(System.in);
+		     System.out.println("How tall are you in Feet?");
+	       	 return userInput.nextDouble();
+	    	}
+	    
+	    public static double inputInches()
+	        {
+	        Scanner userInput =new Scanner(System.in);
+		    System.out.println("Plus how many inches?");
+		    return userInput.nextDouble();
+	        }
+	    public static double convertToInches()
+		    {
+		    	double inches = justInches * 12;
+		    }
+	    public static double convertToCentimeters()
+		    {
+		    	
+		    }
+	    public static double convertToMeters()
+		    {
+		    	
+		    }
+	    public static double outputFeet()
+		    {
+		    	
+		    }
+	    public static double outputInches()
+		    {
+		    	
+		    }
+}
